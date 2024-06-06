1072636baef7f6836ec0e850757758ede52096ed
Boran-Antunna <yakasirivamsivamsivamsi7@gmail.com>
2024-06-06 11:00:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002188401024"))
 	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
 	SHORTLINK_API = os.environ.get('SHORTLINK_API')
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6708929009"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "7159128525"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://developervro:developerrapuka@cluster9.gcj9754.mongodb.net/?retryWrites=true&w=majority&appName=Cluster9")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002088637887")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002213410383")
